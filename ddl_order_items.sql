2def598c9892af1967f9c5056a413a6c88117861
Raghvendra Kuncharapu <kraghavendra079@gmail.com>
2021-09-23 23:24:00 +0530

    tables

---
+++ ddl_order_items.sql
@@ -0,0 +1,18 @@
+DROP TABLE HR.order_items
+/
+CREATE TABLE hr.order_items
+    ( order_id           NUMBER(12) 
+    , line_item_id       NUMBER(3)  NOT NULL
+    , product_id         NUMBER(6)  NOT NULL
+    , unit_price         NUMBER(8,2)
+    , quantity           NUMBER(8)
+    ) 
+TABLESPACE USERS
+/
+
+CREATE OR REPLACE PUBLIC SYNONYM order_items FOR HR.order_items
+/
+GRANT SELECT,INSERT,UPDATE,DELETE ON HR.order_items TO HR;
+/
+
+SHOW ERRORS;
