5b3d60d1a49501f2f9b01082384930e5cb5acb63
Raghvendra Kuncharapu <Raghavendra_Kuncharapu@gap.com>
2021-10-07 22:30:29 +0530

    Update dml_customers.sql

--- dml_customers.sql
+++ dml_customers.sql
@@ -5,4 +5,11 @@ customer_id
 ,cust_address)
 Values (1,'Cardinal','Tom B.','Skagen 21');
 
+INSERT INTO hr.customers (
+customer_id     
+,cust_first_name 
+,cust_last_name  
+,cust_address)
+Values (2,'Rajeev','Sharma','Hyd 21');
+
 COMMIT;
